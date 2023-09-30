724dfd531055c5dbe4ef70dd34410262649e46a0
bkkom <seoy1108@gmail.com>
2023-09-30 14:48:34 +0900

    chore: form error 확인

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -6,12 +6,19 @@ interface IForm {
 }
 
 const CountriesList = ({ title }: { title: string }) => {
-  const { register } = useForm<IForm>();
+  const { register, handleSubmit, formState } = useForm<IForm>();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <>
       <h1>{title}</h1>
-      <form>
-        <input {...register('country')} placeholder="Write a country"></input>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('country', { required: '필수 항목입니다.' })}
+          placeholder="Write a country"
+        ></input>
         <button>Add</button>
       </form>
     </>
