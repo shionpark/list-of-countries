9f94333b4aeaf086ba74260b25c54141658dbadb
bkkom <seoy1108@gmail.com>
2023-09-30 14:51:13 +0900

    feat: useForm 옵션 추가

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -6,21 +6,27 @@ interface IForm {
 }
 
 const CountriesList = ({ title }: { title: string }) => {
-  const { register, handleSubmit, formState } = useForm<IForm>();
-  const onValid = (data: any) => {
-    console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log(data.country);
+    setValue('country', '');
   };
-  console.log(formState.errors);
   return (
     <>
       <h1>{title}</h1>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
           {...register('country', { required: '필수 항목입니다.' })}
           placeholder="Write a country"
         ></input>
         <button>Add</button>
       </form>
+      <span>{errors?.country?.message}</span>
     </>
   );
 };
