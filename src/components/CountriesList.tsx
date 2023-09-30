2628ca784f1833a274692957ce1bf61a0f8f0881
bkkom <seoy1108@gmail.com>
2023-09-30 16:48:40 +0900

    feat: atom 생성

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -1,32 +1,59 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { atom, useRecoilState } from 'recoil';
+
+const INPUT_NAME = 'country';
 
 interface IForm {
   country: string;
 }
 
+interface ICountry {
+  text: string;
+  id: number;
+  category: 'WANNA_GO' | 'HAVE_BEEN' | 'FAVS';
+}
+
+const countryState = atom<ICountry[]>({
+  key: INPUT_NAME,
+  default: [],
+});
+
 const CountriesList = ({ title }: { title: string }) => {
+  const [countries, setCountries] = useRecoilState(countryState);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     setValue,
   } = useForm<IForm>();
-  const handleValid = (data: IForm) => {
-    console.log(data.country);
-    setValue('country', '');
+
+  const handleValid = ({ country }: IForm) => {
+    console.log(country);
+    setCountries((oldValue) => [
+      { text: country, id: Date.now(), category: 'WANNA_GO' },
+      ...oldValue,
+    ]);
+    setValue(INPUT_NAME, '');
   };
+
   return (
     <>
       <h1>{title}</h1>
       <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register('country', { required: '필수 항목입니다.' })}
+          {...register(INPUT_NAME, { required: '필수 항목입니다.' })}
           placeholder="Write a country"
-        ></input>
+        />
         <button>Add</button>
       </form>
       <span>{errors?.country?.message}</span>
+      <ul>
+        {countries.map((i) => (
+          <li key={i.id}>{i.text}</li>
+        ))}
+      </ul>
     </>
   );
 };
