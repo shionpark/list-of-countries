8e64ff6f4b3f323c190c4751597cd7f2138852b7
bkkom <seoy1108@gmail.com>
2023-10-02 17:33:21 +0900

    feat: recoil persist

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,5 @@
-import { atom, selector, selectorFamily } from 'recoil';
+import { atom, selectorFamily } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 import { INPUT_NAME } from '@/constants';
 
 export enum Categories {
@@ -10,12 +11,18 @@ export enum Categories {
 export interface ICountry {
   text: string;
   id: number;
-  category: Categories;
+  category: string;
 }
+
+const { persistAtom } = recoilPersist({
+  key: 'localStorage',
+  storage: localStorage,
+});
 
 export const countryState = atom<ICountry[]>({
   key: INPUT_NAME,
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const categoryState = atom<Categories>({
